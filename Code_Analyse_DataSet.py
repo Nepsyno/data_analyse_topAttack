7709770f0536f55727a9f0a808f1012f0081a2f2
Nepsyno <nepsy.privacy56@protonmail.com>
2025-11-20 18:01:41 +0100

    add initial project files and data analysis scripts

---
+++ Code_Analyse_DataSet.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+df = pd.read_csv('champions-league-2024-UTC.csv', sep=';')
+df_attaquants = df[
+    (df['Pos'].str.contains('FW', na=False)) & 
+    (df['Min'] >= 300)
+].copy()
+
+print(f"Dataset initial : {df.shape}")
+print(f"Dataset filtré (Attaquants) : {df_attaquants.shape}")
+
+cols_to_keep = [
+    'Player', 'Squad', 'Age',       # Identité
+    'Goals', 'Shots', 'SoT',        # Finition brute
+    'SoT%', 'Assists',              # Précision et Altruisme
+    'SCA', 'GCA',                   # Création (Shot/Goal Creating Actions)
+    'TouAttPen',                    # Présence : Touches dans la surface de réparation
+    'CarProg'                       # Percussion : Conduites de balle progressives
+]
+
+df_final = df_attaquants[cols_to_keep].copy()
+
+rename_dict = {
+    'Player': 'Joueur',
+    'Squad': 'Equipe',
+    'Goals': 'Buts',
+    'Shots': 'Tirs_Total',
+    'SoT': 'Tirs_Cadres',
+    'SoT%': 'Tirs_Cadres_Pct',
+    'Assists': 'Passes_Decisives',
+    'SCA': 'Actions_Creation_Tir',
+    'GCA': 'Actions_Creation_But',
+    'TouAttPen': 'Touches_Surface',
+    'CarProg': 'Percussions_Progressives'
+}
+
+df_final = df_final.rename(columns=rename_dict)
+df_final = df_final.reset_index(drop=True)
+display(df_final.head())
+
+df_final.to_csv('top_attaquants_data.csv', index=False)
